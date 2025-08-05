4bcc536ab5082715c6ac37ee37e35ecf33e376cd
Brian McFee <brian.mcfee@nyu.edu>
2025-08-05 12:53:53 -0400

    blacking for lint

--- mir_eval/display.py
+++ mir_eval/display.py
@@ -1312,7 +1312,7 @@ def chords(
             style["edgecolor"] = __chord_to_color(
                 lab, cmap_other, cmap_other, cmap_other
             )
-#            style.setdefault("hatch_linewidth", 3)
+        #            style.setdefault("hatch_linewidth", 3)
         style["fill"] = True
 
         # Swap color -> facecolor here so we preserve edgecolor on rects
